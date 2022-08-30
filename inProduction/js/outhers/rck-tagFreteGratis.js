af219fdb5115c535b0c010dd15ddd91383d029d5
Paulo Geliandes <geliandes@hotmail.com>
2022-08-30 13:51:51 -0300

    update to use the same progress bar value
    update to use the same progress bar value for shipping

--- inProduction/js/outhers/rck-tagFreteGratis.js
+++ inProduction/js/outhers/rck-tagFreteGratis.js
@@ -1,13 +1,12 @@
 function tagFrete() {
 
     // Valor mínimo para ser aplicado a tag de frete grátis
-    let valorMinimo = 350.00;
-
+    let valorMinimo = parseFloat(document.querySelector('.menu__frete').innerText) / 100;
     let createElementDiv, priceValidated;
 	let spanFreteGratis = `<span>Frete grátis</span>`;
 
-	// obtenção do valor e verificação se é maior que R$350, caso seja será adicionado o span de Frete Grátis
-	function freteGratisFunction() {
+	// obtenção do valor e verificação se é maior que o valor mínimo para o frete grátis, caso seja será adicionado o span de Frete Grátis
+	function freteGratisTag() {
 
 		for (let i = 0; i < document.getElementsByClassName('productCard__brand').length; i++) {
 
@@ -21,35 +20,33 @@ function tagFrete() {
 				createElementDiv.classList.add('tagFreteGratis');
 				createElementDiv.innerHTML = spanFreteGratis;
 				document.getElementsByClassName('productCard__brand')[i].appendChild(createElementDiv);
-			}
-		}
-	}
+			};
+		};
+	};
 
 	// Corrigindo possíveis duplicidades de atribuição
 	function freteGratisFix() {
 		for (let i = 0; i < document.getElementsByClassName('productCard__brand').length; i++) {
 
-			let fixDuplicityFrete = document.querySelectorAll("span[class='productCard__brand']")[i]
+			let fixDuplicityFrete = document.querySelectorAll("span[class='productCard__brand']")[i];
 
 			if (fixDuplicityFrete.children[1] != undefined) {
 				if (fixDuplicityFrete.children[0].innerHTML == fixDuplicityFrete.children[1].innerHTML) {
-					fixDuplicityFrete.children[1].remove()
-				}
-			}
-		}
-	}
+					fixDuplicityFrete.children[1].remove();
+				};
+			};
+		};
+	};
 
 	//Mutation Observer para caso navegue para outra paginação o span seja adicionado aos produtos
 	const productsObserverved = document.querySelector("ul[class='vitrine__group']");
 
 	if (productsObserverved != null) {
 		const observerProducts = new MutationObserver(() => {
-			freteGratisFunction()
-			freteGratisFix()
-		})
-		observerProducts.observe(productsObserverved, {
-			childList: true
+			freteGratisTag();
+			freteGratisFix();
 		});
+		observerProducts.observe(productsObserverved, {childList: true});
 	}
 
 	//Mutation Observer para cards da smarthint da HOME
@@ -64,21 +61,17 @@ function tagFrete() {
 
 				if (childrenHomeObserved != null) {
 					const observerChildrenHome = new MutationObserver(() => {
-						freteGratisFunction()
-						freteGratisFix()
-					})
-
-					observerChildrenHome.observe(childrenHomeObserved, {
-						attributes: true
+						freteGratisTag();
+						freteGratisFix();
 					});
-				}
-			}
-		})
 
-		observerHome.observe(homeObserved, {
-			childList: true
+					observerChildrenHome.observe(childrenHomeObserved, {attributes: true});
+				};
+			};
 		});
-	}
+
+		observerHome.observe(homeObserved, {childList: true});
+	};
     
 	//Mutation Observer para pesquisa dinâmica da SmartHint (incluindo o "veja mais")
 	const searchObserved = document.querySelector("html");
@@ -86,25 +79,21 @@ function tagFrete() {
 	if (searchObserved != null) {
 
 		const observerSearch = new MutationObserver(() => {
-			freteGratisFunction()
-			freteGratisFix()
+			freteGratisTag();
+			freteGratisFix();
 
 			const searchViewMoreObserved = document.querySelector("div[id='smarthint-search-products']");
 
 			if (searchViewMoreObserved != null) {
 				const observerSearchViewMore = new MutationObserver(() => {
-					freteGratisFunction()
-					freteGratisFix()
-				})
-				observerSearchViewMore.observe(searchViewMoreObserved, {
-					childList: true
-				})
-			}
-		})
-		observerSearch.observe(searchObserved, {
-			attributes: true
-		})
-	}
+					freteGratisTag();
+					freteGratisFix();
+				});
+				observerSearchViewMore.observe(searchViewMoreObserved, {childList: true});
+			};
+		});
+		observerSearch.observe(searchObserved, {attributes: true});
+	};
 }
 
 window.addEventListener("DOMContentLoaded", tagFrete());
